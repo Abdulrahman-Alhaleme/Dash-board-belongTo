4fd54add1dee1eb5b05d8d3c11715e7ed35636f8
Abdulrahman-Alhaleme <abdulrahman.alhaleme@gmail.com>
2023-06-17 02:01:24 +0300

    update

---
+++ src/assets/dummy-data/navLinks.js
@@ -0,0 +1,24 @@
+const navLinks = [
+  {
+    path: "/dashboard",
+    icon: "ri-apps-2-line",
+    display: " لوحة التحكم",
+  },
+  {
+    path: "/bookings",
+    icon: "ri-emotion-happy-fill",
+    display: "المستخدمين",
+  },
+  {
+    path: "/sell-car",
+    icon: "ri-emotion-laugh-fill",
+    display: "المتبرعين",
+  },
+  {
+    path: "/settings",
+    icon: "ri-settings-2-line",
+    display: "الأعدادات",
+  },
+];
+
+export default navLinks;
